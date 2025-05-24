a82da61a460e66866d27b754e1ddb2757e727fe8
NoName <xaviferrer2011@gmail.com>
2025-05-24 14:54:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 let htmlEditor, cssEditor, jsEditor;
 
-window.onload = function () {
-  htmlEditor = CodeMirror.fromTextArea(document.getElementById("htmlEditor"), {
-    mode: "xml",
-    lineNumbers: true,
-    theme: "default"
+ const editors = {};
+    const modes = {
+      html: 'xml',
+      css: 'css',
+      js: 'javascript'
+    };
+
+    document.querySelectorAll('textarea.code-tab').forEach(el => {
+      const id = el.id;
+      editors[id] = CodeMirror.fromTextArea(el, {
+        mode: modes[id],
+        lineNumbers: true,
+        theme: 'default'
+      });
+    });
+
+  function showTab(tab) {
+  Object.keys(editors).forEach(key => {
+    const wrapper = editors[key].getWrapperElement();
+    if (key === tab) {
+      wrapper.classList.remove('hidden');
+    } else {
+      wrapper.classList.add('hidden');
+    }
   });
 
-  cssEditor = CodeMirror.fromTextArea(document.getElementById("cssEditor"), {
-    mode: "css",
-    lineNumbers: true,
-    theme: "default"
-  });
-
-  jsEditor = CodeMirror.fromTextArea(document.getElementById("jsEditor"), {
-    mode: "javascript",
-    lineNumbers: true,
-    theme: "default"
-  });
-
-  document.getElementById("runBtn").addEventListener("click", runCode);
-};
-
-function runCode() {
-  const html = htmlEditor.getValue();
-  const css = cssEditor.getValue();
-  const js = jsEditor.getValue();
-
-  const result = `
-    <html>
-      <head><style>${css}</style></head>
-      <body>
-        ${html}
-        <script>${js}<\/script>
-      </body>
-    </html>
-  `;
-
-  const outputFrame = document.getElementById("output");
-  outputFrame.srcdoc = result;
+  document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+  document.querySelector(`.tab-button[onclick*="${tab}"]`).classList.add('active');
 }
+
+
+
+    function runCode() {
+      const html = editors['html'].getValue();
+      const css = `<style>${editors['css'].getValue()}</style>`;
+      const js = `<script>${editors['js'].getValue()}<\/script>`;
+
+      const output = html + css + js;
+      const iframe = document.getElementById('preview');
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+      doc.open();
+      doc.write(output);
+      doc.close();
+    }
+
+   document.querySelectorAll('textarea.code-tab').forEach(el => {
+  const id = el.id;
+  editors[id] = CodeMirror.fromTextArea(el, {
+    mode: modes[id],
+    lineNumbers: true,
+    theme: 'default'
+  });
+
+  const wrapper = editors[id].getWrapperElement();
+  if (id === 'html') {
+    wrapper.classList.add('visible');
+  } else {
+    wrapper.classList.remove('visible');
+  }
+});
+
 
