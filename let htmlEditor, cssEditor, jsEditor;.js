1f38a6487b41b8c03ffb2f5a06ed4873eef0b6a2
NoName <xaviferrer2011@gmail.com>
2025-05-29 17:37:06 +0200

    Update let htmlEditor, cssEditor, jsEditor;.js

--- let htmlEditor, cssEditor, jsEditor;.js
+++ let htmlEditor, cssEditor, jsEditor;.js
@@ -1,32 +1,23 @@
-let htmlEditor, cssEditor, jsEditor;
+const editors = {};
 
- const editors = {
-  html: CodeMirror.fromTextArea(document.getElementById('html'), {
+window.onload = () => {
+  editors.html = CodeMirror.fromTextArea(document.getElementById('html'), {
     mode: 'xml',
-    lineNumbers: true,
-    theme: 'default'
-  }),
-  css: CodeMirror.fromTextArea(document.getElementById('css'), {
+    lineNumbers: true
+  });
+
+  editors.css = CodeMirror.fromTextArea(document.getElementById('css'), {
     mode: 'css',
-    lineNumbers: true,
-    theme: 'default'
-  }),
-  js: CodeMirror.fromTextArea(document.getElementById('js'), {
+    lineNumbers: true
+  });
+
+  editors.js = CodeMirror.fromTextArea(document.getElementById('js'), {
     mode: 'javascript',
-    lineNumbers: true,
-    theme: 'default'
-  })
+    lineNumbers: true
+  });
+
+  showTab('html');
 };
-
-
-    document.querySelectorAll('textarea.code-tab').forEach(el => {
-      const id = el.id;
-      editors[id] = CodeMirror.fromTextArea(el, {
-        mode: modes[id],
-        lineNumbers: true,
-        theme: 'default'
-      });
-    });
 
 function showTab(tab) {
   Object.keys(editors).forEach(key => {
@@ -35,37 +26,20 @@ function showTab(tab) {
   });
 
   document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
-  document.querySelector(`.tab-button[data-tab="${tab}"]`).classList.add('active');
+  document.querySelector(`.tab-button[onclick*="${tab}"]`).classList.add('active');
 }
 
+function runCode() {
+  const html = editors.html.getValue();
+  const css = `<style>${editors.css.getValue()}</style>`;
+  const js = `<script>${editors.js.getValue()}<\/script>`;
 
+  const output = html + css + js;
+  const iframe = document.getElementById('preview');
+  const doc = iframe.contentDocument || iframe.contentWindow.document;
+  doc.open();
+  doc.write(output);
+  doc.close();
+}
 
-    function runCode() {
-      const html = editors['html'].getValue();
-      const css = `<style>${editors['css'].getValue()}</style>`;
-      const js = `<script>${editors['js'].getValue()}<\/script>`;
-
-      const output = html + css + js;
-      const iframe = document.getElementById('preview');
-      const doc = iframe.contentDocument || iframe.contentWindow.document;
-      doc.open();
-      doc.write(output);
-      doc.close();
-    }
-
-   document.querySelectorAll('textarea.code-tab').forEach(el => {
-  const id = el.id;
-  editors[id] = CodeMirror.fromTextArea(el, {
-    mode: modes[id],
-    lineNumbers: true,
-    theme: 'default'
-  });
-
-  const wrapper = editors[id].getWrapperElement();
-  if (id === 'html') {
-    wrapper.classList.add('visible');
-  } else {
-    wrapper.classList.remove('visible');
-  }
-});
 
